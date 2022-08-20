c33e8f55e07025ab3824c46a57e32ba19e13a14c
Daris02 <hei.raymond.2@gmail.com>
2022-08-20 13:17:39 +0300

    add the project v0.0

---
+++ note-hei/src/main/java/com/hei/notehei/NoteHeiApplication.java
@@ -0,0 +1,13 @@
+package com.hei.notehei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteHeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteHeiApplication.class, args);
+	}
+
+}
