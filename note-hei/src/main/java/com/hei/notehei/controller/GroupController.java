1d9cd9f4281d4179a0e2158e35b7e2a2906894e4
Daris02 <hei.raymond.2@gmail.com>
2022-08-26 12:47:44 +0300

    Update Group

---
+++ note-hei/src/main/java/com/hei/notehei/controller/GroupController.java
@@ -0,0 +1,39 @@
+package com.hei.notehei.controller;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.hei.notehei.model.Group;
+import com.hei.notehei.service.GroupService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class GroupController {
+    private GroupService groupService;
+
+    @GetMapping("/groups")
+    public List<Group> getAllGroups(){
+        return groupService.getAllGroups();
+    }
+
+    @GetMapping("/group/{id}")
+    public Optional<Group> getGroupById(@PathVariable Long id){
+        return groupService.getGroup(id); 
+    }
+
+    @PostMapping("/group")
+    public Group addGroupById(@RequestBody Group group){
+        return groupService.addGroup(group);
+    }
+
+    @DeleteMapping("/group/{id}")
+    public void deleteGroupById(@PathVariable Long id){
+        groupService.deletGroup(id);
+    }
+}
---
+++ note-hei/src/main/java/com/hei/notehei/service/GroupService.java
@@ -0,0 +1,35 @@
+package com.hei.notehei.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+import com.hei.notehei.model.Group;
+import com.hei.notehei.repository.GroupRepository;
+
+@Service
+@AllArgsConstructor
+public class GroupService {
+    @Autowired
+    private GroupRepository groupRepository;
+
+    public List<Group> getAllGroups(){
+        return groupRepository.findAll(); 
+    }
+
+    public Optional<Group> getGroup(Long id){
+        return groupRepository.findById(id);
+    }
+
+    public Group addGroup(Group group){
+        return groupRepository.save(group);
+    }
+
+    public void deletGroup(Long id){
+        groupRepository.deleteById(id);
+    }
+}
