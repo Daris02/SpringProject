15fe3925bb32e47bc7f7c8ebdcbe7bc55e751f4a
Daris02 <hei.raymond.2@gmail.com>
2022-08-31 16:00:02 +0300

    feat: add requestmapping

--- note-hei/src/main/java/com/hei/notehei/NoteHeiApplication.java
+++ note-hei/src/main/java/com/hei/notehei/NoteHeiApplication.java
@@ -1,12 +1,28 @@
 package com.hei.notehei ;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hei.notehei.model.Subject;
+import com.hei.notehei.repository.SubjectRepository;
+
+
 @SpringBootApplication
-public class NoteHeiApplication {
+public class NoteHeiApplication implements CommandLineRunner{
+	@Autowired
+	private SubjectRepository subjectRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(NoteHeiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		subjectRepository.save( new Subject((long) 1, "PROG1"));
+		
+	}
+
+
 
 }
--- note-hei/src/main/java/com/hei/notehei/Reception/HomePage.java
+++ note-hei/src/main/java/com/hei/notehei/Reception/HomePage.java
@@ -1,15 +1,32 @@
 package com.hei.notehei.Reception;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hei.notehei.model.Group;
+import com.hei.notehei.repository.GroupRepository;
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class HomePage {
+    @Autowired
+    GroupRepository groupRepository;
 
-    @GetMapping
+    @RequestMapping
     public String getHomePage(Model model){
-        model.addAttribute("somthing", "Welcome home page of my Spring Project");
         return "index" ;
+    }
+
+    @RequestMapping("/groupe")
+    public String groupe(Model model){
+        List<Group> listGroupe = groupRepository.findAll();
+        model.addAttribute("listGroupe", listGroupe);
+        return "groupe";
     }
 }
