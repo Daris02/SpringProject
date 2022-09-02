6a42680f44a3e2bd9e12f6b225e0add1d9017aee
Daris02 <hei.raymond.2@gmail.com>
2022-09-02 07:50:31 +0300

    feat: add query method

--- note-hei/src/main/java/com/hei/notehei/repository/ExamenRepository.java
+++ note-hei/src/main/java/com/hei/notehei/repository/ExamenRepository.java
@@ -1,11 +1,18 @@
 package com.hei.notehei.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hei.notehei.model.Examen;
 
 @Repository
 public interface ExamenRepository extends JpaRepository<Examen, Long>{
-    //
+    
+    @Query("select e from examen e where upper(e.title) like upper(:x) ")
+    Page<Examen> search(@Param("x") String e, Pageable pageable);
+
 }
--- note-hei/src/main/java/com/hei/notehei/repository/SubjectRepository.java
+++ note-hei/src/main/java/com/hei/notehei/repository/SubjectRepository.java
@@ -1,11 +1,18 @@
 package com.hei.notehei.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hei.notehei.model.Subject;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long>{
-    //
+    
+    @Query("select sb from subjects sb where upper(sb.name) like upper(:x) ")
+    Page<Subject> search(@Param("x") String sb, Pageable pageable);
+
 }
