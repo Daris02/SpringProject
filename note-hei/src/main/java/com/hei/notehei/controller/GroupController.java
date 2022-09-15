c5ee3ad87f3c0c082b53216a51422d03d5d99a7e
VOLABEMANANA <hei.fabricia.2@gmail.com>
2022-09-15 12:54:24 +0300

    fix: change

--- note-hei/src/main/java/com/hei/notehei/controller/GroupController.java
+++ note-hei/src/main/java/com/hei/notehei/controller/GroupController.java
@@ -1,4 +1,4 @@
-package com.hei.notehei.Controller;
+package com.hei.notehei.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
