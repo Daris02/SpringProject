39a379311c739d108502f91dbfb6dd96e815954a
Daris02 <hei.raymond.2@gmail.com>
2022-09-15 14:38:36 +0300

    style: update

--- note-hei/src/main/java/com/hei/notehei/Reception/HomePage.java
+++ note-hei/src/main/java/com/hei/notehei/Reception/HomePage.java
@@ -1,21 +1,35 @@
 package com.hei.notehei.Reception;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hei.notehei.repository.GroupRepository;
+import com.hei.notehei.repository.StudentRepository;
 
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class HomePage {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
 
     @GetMapping
-    public String getHome(){
+    public String getHome(Model model){
+        model.addAttribute("numberStudent", studentRepository.count());
+        model.addAttribute("numberGroup", groupRepository.count());
         return "index" ;
     }
 
     @GetMapping("/index")
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        model.addAttribute("numberStudent", studentRepository.count());
+        model.addAttribute("numberGroup", groupRepository.count());
         return "index" ;
     }
 }
